f9d2654e155d0d06d15245d627604811166af1d3
Asidert <612boris40@gmail.com>
2026-07-20 17:59:23 +0400

    Update load_deps.sh

--- load_deps.sh
+++ load_deps.sh
@@ -49,6 +49,10 @@ if [ ! -f "$COMFYUI_DIR/custom_nodes/.custom_deps_installed" ]; then
     echo "Downloaded $COUNT character LoRA(s)"
 
     curl --fail --retry 5 --retry-max-time 0 -C - -L \
+        -o "$COMFYUI_DIR/models/loras/zit/hands_feet_skin_v1.1.safetensors" \
+        "https://flammaverse.com/loras/models/loras/zit/hands_feet_skin_v1.1.safetensors"
+
+    curl --fail --retry 5 --retry-max-time 0 -C - -L \
         -o "$COMFYUI_DIR/models/loras/zit/better_images_loraholic.safetensors" \
         "https://flammaverse.com/loras/models/loras/zit/better_images_loraholic.safetensors"
         
